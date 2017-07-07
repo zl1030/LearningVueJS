fe85f92b08c4c7e33b5a128b3a6d7b0ba13e8e0e
zl1030 <651426288@qq.com>
2017-07-08 00:43:47 +0800

    表单控件测试

--- src/components/operationData/NewPlayer.vue
+++ src/components/operationData/NewPlayer.vue
@@ -1,75 +1,112 @@
 <template>
-  <!--<div class="hello">-->
   <section class="chart-container">
     <el-row>
       <el-col :span="24">
         <h1>{{ msg }}</h1>
+        <h2>{{beginDate | formatDate}}-{{endDate | formatDate}}</h2>
+        <h2>{{channelId}}</h2>
+        <h2>{{serverId}}</h2>
       </el-col>
-      <el-col :span="24">
-        <h2>Hello {{ username }}</h2>
-        <h2>开关状态:{{value1}}</h2>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <span>渠道</span>
+        <el-select v-model="channelId" placeholder="请选择">
+          <el-option
+            v-for="item in channels"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
       </el-col>
+      <el-col :span="6">
+        <span>游戏区</span>
+        <el-select v-model="serverId" placeholder="请选择">
+          <el-option
+            v-for="item in servers"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="6">
+        <div class="block">
+          <span class="demonstration">起始时间</span>
+          <el-date-picker
+            v-model="beginDate"
+            type="datetime"
+            placeholder="选择起始时间">
+          </el-date-picker>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="block">
+          <span class="demonstration">结束时间</span>
+          <el-date-picker
+            v-model="endDate"
+            type="datetime"
+            placeholder="选择结束时间">
+          </el-date-picker>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
       <el-col :span="12">
         <el-button type="primary" @click.native.prevent="requestData1">请求1</el-button>
       </el-col>
       <el-col :span="12">
         <el-button type="primary" @click.native.prevent="requestData2">请求2</el-button>
       </el-col>
+    </el-row>
+    <el-row>
       <el-col :span="12">
-        <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
-      </el-col>
-      <el-col :span="12">
-        <el-switch
-          v-model="value1"
-          on-text=""
-          off-text="">
-        </el-switch>
+        <div id="myChart" :style="{width: '100%', height: '300px'}"></div>
       </el-col>
     </el-row>
   </section>
-  <!--</div>-->
 </template>
 
 <script>
-  import { requestLogin } from '../../api/api'
-  import ElRow from 'element-ui/packages/row/src/row'
+  import { requestLogin } from '@/api/api'
+  import { Servers, Channels } from '@/consts'
   import ElCol from 'element-ui/packages/col/src/col'
+  import ElRow from 'element-ui/packages/row/src/row'
+  import { formatDate } from '@/utils'
 
   export default {
     components: {
-      ElCol,
-      ElRow
+      ElRow,
+      ElCol
     },
     name: 'hello',
     data () {
       return {
         msg: '新增玩家',
-        value1: true,
-        username: 'unknown',
+        beginDate: this.getNowFormatDate(),
+        endDate: this.getNowFormatDate(),
         chartData: [],
-        myChart: null
+        myChart: null,
+        servers: Servers,
+        channels: Channels,
+        channelId: -1,
+        serverId: -1
       }
     },
     mounted () {
-      var user = sessionStorage.getItem('access-user')
-      if (user) {
-        user = JSON.parse(user)
-        console.log('user:' + user)
-        this.username = user.name || ''
-      }
       // 获取页面上的图形DOM对象
       this.myChart = this.$echarts.init(document.getElementById('myChart'))
       this.chartRepaint()
     },
+    filters: {
+      formatDate (time) {
+        var date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm:ss')
+      }
+    },
     methods: {
-      logout () {
-        var _this = this
-        this.$confirm('确认退出吗?', '提示', {}).then(() => {
-          _this.$router.push('/login')
-        }).catch(() => {
-
-        })
-      },
       // 绘制图表
       chartRepaint () {
         this.myChart.setOption({
@@ -101,6 +138,23 @@
           this.chartData = [2, 5, 2, 5, 2, 5]
           this.chartRepaint()
         })
+      },
+      getNowFormatDate () {
+        var date = new Date()
+        var seperator1 = '-'
+        var seperator2 = ':'
+        var month = date.getMonth() + 1
+        var strDate = date.getDate()
+        if (month >= 1 && month <= 9) {
+          month = '0' + month
+        }
+        if (strDate >= 0 && strDate <= 9) {
+          strDate = '0' + strDate
+        }
+        var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
+          ' ' + date.getHours() + seperator2 + date.getMinutes() +
+          seperator2 + date.getSeconds()
+        return currentdate
       }
     }
   }
@@ -129,5 +183,15 @@
   .chart-container {
     width: 100%;
     float: left;
+  }
+
+  .el-row {
+    margin-bottom: 20px;
+
+  &
+  :last-child {
+    margin-bottom: 0;
+  }
+
   }
 </style>
---
+++ src/consts/index.js
@@ -0,0 +1,43 @@
+/**
+ * Created by zl1030 on 2017/7/7.
+ */
+
+export const Servers = [{
+  value: -1,
+  label: '所有'
+}, {
+  value: 1,
+  label: '区1'
+}, {
+  value: 2,
+  label: '区2'
+}, {
+  value: 3,
+  label: '区3'
+}, {
+  value: 4,
+  label: '区4'
+}, {
+  value: 5,
+  label: '区5'
+}]
+
+export const Channels = [{
+  value: -1,
+  label: '全部'
+}, {
+  value: 1,
+  label: '黄金糕'
+}, {
+  value: 2,
+  label: '双皮奶'
+}, {
+  value: 3,
+  label: '蚵仔煎'
+}, {
+  value: 4,
+  label: '龙须面'
+}, {
+  value: 5,
+  label: '北京烤鸭'
+}]
