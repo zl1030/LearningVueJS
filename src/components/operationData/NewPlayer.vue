baa3d90bafb95966be19f8793090647014ad8c99
zhoulei <zhoulei@xuanxiaotec.com>
2017-07-07 18:30:36 +0800

    试着用el-row,el-col改了下布局

--- src/components/operationData/NewPlayer.vue
+++ src/components/operationData/NewPlayer.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Hello {{ username }}</h2>
-    <h2>开关状态:{{value1}}</h2>
-    <el-button type="primary" @click.native.prevent="requestData1">请求1</el-button>
-    <el-button type="primary" @click.native.prevent="requestData2">请求2</el-button>
-    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
-    <el-switch
-      v-model="value1"
-      on-text=""
-      off-text="">
-    </el-switch>
-  </div>
+  <!--<div class="hello">-->
+  <section class="chart-container">
+    <el-row>
+      <el-col :span="24">
+        <h1>{{ msg }}</h1>
+      </el-col>
+      <el-col :span="24">
+        <h2>Hello {{ username }}</h2>
+        <h2>开关状态:{{value1}}</h2>
+      </el-col>
+      <el-col :span="12">
+        <el-button type="primary" @click.native.prevent="requestData1">请求1</el-button>
+      </el-col>
+      <el-col :span="12">
+        <el-button type="primary" @click.native.prevent="requestData2">请求2</el-button>
+      </el-col>
+      <el-col :span="12">
+        <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+      </el-col>
+      <el-col :span="12">
+        <el-switch
+          v-model="value1"
+          on-text=""
+          off-text="">
+        </el-switch>
+      </el-col>
+    </el-row>
+  </section>
+  <!--</div>-->
 </template>
 
 <script>
   import { requestLogin } from '../../api/api'
+  import ElRow from 'element-ui/packages/row/src/row'
+  import ElCol from 'element-ui/packages/col/src/col'
 
   export default {
+    components: {
+      ElCol,
+      ElRow
+    },
     name: 'hello',
     data () {
       return {
@@ -51,7 +73,7 @@
       // 绘制图表
       chartRepaint () {
         this.myChart.setOption({
-          title: {text: '在Vue中使用echarts'},
+          title: {text: '图表测试1'},
           tooltip: {},
           xAxis: {
             data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
@@ -102,5 +124,10 @@
 
   a {
     color: #42b983;
+  }
+
+  .chart-container {
+    width: 100%;
+    float: left;
   }
 </style>
